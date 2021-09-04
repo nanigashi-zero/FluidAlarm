4292b5f966969989a1c395a06470b3e46d222140
nanigashi-zero <nanigashi08x@gmail.com>
2021-09-04 19:35:12 +0900

    Initial Commit

---
+++ FluidAlarm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FluidAlarm
+//
+//  Created by KoseiKarasaki on 2021/09/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FluidAlarm/FluidAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluidAlarmApp.swift
+//  FluidAlarm
+//
+//  Created by KoseiKarasaki on 2021/09/04.
+//
+
+import SwiftUI
+
+@main
+struct FluidAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
